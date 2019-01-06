258c8eec52e2ead4315d3d722ef8ce848778864b
atqnp <atiq_np@hotmail.com>
2019-01-06 12:47:59 +0900

    restore credentials

--- app.py
+++ app.py
@@ -13,7 +13,23 @@ from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 scope = ['https://spreadsheets.google.com/feeds',
 		 'https://www.googleapis.com/auth/drive']
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('gspreadsheet-yuai-638e20a7f7b0.json',scope)
+SHEET_PRIVATE_KEY = os.environ['SHEET_PRIVATE_KEY']
+SHEET_PRIVATE_KEY = SHEET_PRIVATE_KEY.replace('\\n', '\n')
+
+credential = {
+                "type": "service_account",
+                "project_id": os.environ['SHEET_PROJECT_ID'],
+                "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+                "private_key": SHEET_PRIVATE_KEY,
+                "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+                "client_id": os.environ['SHEET_CLIENT_ID'],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+             }
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential,scope)
 
 UPDATE_INTERVAL = 15
 
